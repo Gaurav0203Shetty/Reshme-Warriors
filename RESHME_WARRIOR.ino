145675cb9621a3cc693caa51c59fc4ceb5c980c6
RasUnder025 <148210176+RasUnder025@users.noreply.github.com>
2024-09-28 09:24:37 +0530

    Update RESHME_WARRIOR.ino

--- RESHME_WARRIOR.ino
+++ RESHME_WARRIOR.ino
@@ -1,32 +1,34 @@
+/**
+This Arduino sketch is the code we executed on our offline live demonstration using Arduino UNO.
+  **/
+
 #include "DHT.h"
 
-int DHT11_PIN =A2; 
+int DHT11_PIN =A2;  //input pin for DHT sensor
 DHT dht11(A2, DHT11);
 
-int LED1=2; //output pin 1
-int LED2=3; //output pin 2
-int sensorPin = A0; //input pin for LDR
+int LED1=2; //output pin 2
+int LED2=3; //output pin 3
+int sensorPin = A0; //input pin for LDR sensor
 int sensorValue = 0; // variable to store the value coming from the sensor
 
 void setup() {
-
-  Serial.begin(9600);
   dht11.begin();
   Serial.begin(9600); //sets serial port for communication
-  pinMode(2, OUTPUT);
-  pinMode(3,OUTPUT);
+  pinMode(2, OUTPUT); //set LED for light
+  pinMode(3,OUTPUT); //set LED for temperature
  
 }
 
 void loop() {
-sensorValue = analogRead(sensorPin); // read the value from the sensor
+sensorValue = analogRead(sensorPin); //read the value from the sensor
 Serial.println(sensorValue); //prints the values coming from the sensor on the screen
 
 if (sensorValue<300) {
-    digitalWrite(LED1, HIGH); 
+    digitalWrite(LED1, HIGH); //Dark conditions
  
   } else {
-    digitalWrite(LED1, LOW);
+    digitalWrite(LED1, LOW); //Bright conditions
    
   }
 
@@ -45,7 +47,6 @@ if (isnan(hum) || isnan(tempC) || isnan(tempF)){
     Serial.print("DHT11# Humidity:  ");
     Serial.print(hum);
     Serial.print("%");
-
     Serial.print("|");
     Serial.print("Temperature");
     Serial.print(tempC);
@@ -53,11 +54,11 @@ if (isnan(hum) || isnan(tempC) || isnan(tempF)){
     Serial.print(tempF);
     Serial.println("°F");
     if (tempC < 22 || tempC > 28) {
-       digitalWrite(LED2, HIGH);
+       digitalWrite(LED2, HIGH); //Unsuitable conditions
   } else {
-    digitalWrite(LED2,LOW);
+    digitalWrite(LED2,LOW); //Suitable conditions
   }
 
   }
-  delay(1000);
+  delay(1000); //1 second delay
 }
